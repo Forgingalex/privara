b103acf44fb7e9a8a5ae80b7256833b89e7e664b
Forgingalex <olaiyaalexander97@gmail.com>
2025-11-28 00:45:44 +0100

    Improve global polyfill stub module
    - Better fallback logic for finding global object
    - Ensure window.global is set
    - Export the global object for webpack ProvidePlugin
    - More robust error handling

--- frontend/polyfills/global.js
+++ frontend/polyfills/global.js
@@ -1,19 +1,28 @@
 // Polyfill stub for global - webpack resolves this as a module
 // In Node.js, 'global' is a global variable; in browsers we use window/globalThis
-if (typeof global === 'undefined') {
-  if (typeof globalThis !== 'undefined') {
-    // @ts-ignore
-    global = globalThis;
-  } else if (typeof window !== 'undefined') {
-    // @ts-ignore
-    global = window;
-  } else if (typeof self !== 'undefined') {
-    // @ts-ignore
-    global = self;
-  } else {
-    throw new Error('Unable to polyfill global');
-  }
+// This module is used by ProvidePlugin to inject 'global' wherever it's referenced
+
+var g;
+
+// Try to get the actual global object
+if (typeof globalThis !== 'undefined') {
+  g = globalThis;
+} else if (typeof window !== 'undefined') {
+  g = window;
+} else if (typeof self !== 'undefined') {
+  g = self;
+} else if (typeof global !== 'undefined') {
+  g = global;
+} else {
+  // Last resort - create an object
+  g = {};
 }
 
-module.exports = global;
+// Ensure global is available on the global object itself
+if (typeof window !== 'undefined') {
+  window.global = g;
+}
+
+// Export the global object
+module.exports = g;
 
